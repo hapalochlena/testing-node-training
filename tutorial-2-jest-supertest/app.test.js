ff3986b50e33ebfa07cbeb24f2f87132c3355546
Lena Schneider <lena.schneider407@gmail.com>
2023-04-02 18:18:24 +0200

    first test setup working

--- tutorial-2-jest-supertest/app.test.js
+++ tutorial-2-jest-supertest/app.test.js
@@ -1,13 +1,23 @@
-const supertest = require('supertest')
+const supertest = require('supertest').request() // ! had to add '.request()'
 const app = require('./app')
 
 describe("POST /users", () => {
 
   describe("given a username and password", () => {
-    // should save the username and password to the DB
-    // should respond with a json object containing the user id (of the user we just created)
-    // should respond with a 200 status code
-    // should specify json in the content type header (can be important e.g. when you use high-level library like Axios to convert json into js object)
+    // 1) should save the username and password to the DB // * ignore for now
+    // 2) should respond with a json object containing the user id (of the user we just created) // * ignore for now
+
+    // * Just test the stuff that has to do with the HTTP INTERFACE:
+
+    // 3) should respond with a 200 status code
+    test("should respond with a 200 status code", async () => {
+      const response = await request(app) // 'request' = supertest function; pass in the HTTP server object = in this case 'app' from express
+                              .post('/users') // we want to make a POST request to the /users endpoint...
+                              .send({ username: "username", password: "password" }) // ...sending this data
+      expect(response.statusCode).toBe(200)
+    })
+
+    // 4) should specify json in the content type header (can be important e.g. when you use high-level library like Axios to convert json into js object)
   })
 
   describe("when the username and password is missing", () => {
@@ -17,7 +27,7 @@ describe("POST /users", () => {
 })
 
 
-// REALISTIC IMPLEMENTATION:
+// * 1) + 2) => REALISTIC IMPLEMENTATION:
 // - hash the password before it gets to the DB
 // - send the user id back to the client not as plain text, but store it in a session / encrypted cookie / json web token / ...
 // - handle when the DB fails: 500 status if no connection to DB
