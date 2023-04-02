119d14c06486b4fed1f3acb0d2a02b6b459d35de
Lena Schneider <lena.schneider407@gmail.com>
2023-04-02 17:54:45 +0200

    test setup

--- tutorial-2-jest-supertest/app.js
+++ tutorial-2-jest-supertest/app.js
@@ -1,12 +1,11 @@
 const express = require('express')
-
 const app = express()
 
-// test endpoint that CREATES A NEW USER
+// endpoint that CREATES A NEW USER
 // => user can create a POST request with a USERNAME + PASSWORD
 // => stores that data into the DB
-app.get('/test', (req, res) => {
-  res.send("blaaaaa")
+app.post('/users', (req, res) => {
+  
 })
 
 module.exports = app
--- tutorial-2-jest-supertest/app.test.js
+++ tutorial-2-jest-supertest/app.test.js
@@ -1,5 +1,23 @@
+const supertest = require('supertest')
 const app = require('./app')
 
-test('', () => {
-  
+describe("POST /users", () => {
+
+  describe("given a username and password", () => {
+    // should save the username and password to the DB
+    // should respond with a json object containing the user id (of the user we just created)
+    // should respond with a 200 status code
+    // should specify json in the content type header (can be important e.g. when you use high-level library like Axios to convert json into js object)
+  })
+
+  describe("when the username and password is missing", () => {
+    // should respond with a status code of 400
+  })
+
 })
+
+
+// REALISTIC IMPLEMENTATION:
+// - hash the password before it gets to the DB
+// - send the user id back to the client not as plain text, but store it in a session / encrypted cookie / json web token / ...
+// - handle when the DB fails: 500 status if no connection to DB
