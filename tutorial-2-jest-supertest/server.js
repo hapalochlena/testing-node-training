8c7f1bf68e495d8dc400299cd2bd0cc44dfe1704
Lena Schneider <lena.schneider407@gmail.com>
2023-04-04 12:09:48 +0200

    explanation comment added

--- tutorial-2-jest-supertest/server.js
+++ tutorial-2-jest-supertest/server.js
@@ -1,3 +1,6 @@
+// Why this extra server file? => Jest/Supertest binds the routes to "whatever port it wants"
+// => We put our app.listen(3000) that we would use for manual testing / to actually spin up our server in here, so it doesn't get in the way
+
 const app = require('./app.js')
 
 app.listen(3000, () => console.log("listening on 3000 "));
